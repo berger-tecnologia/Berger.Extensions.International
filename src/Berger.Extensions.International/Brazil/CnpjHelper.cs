8957910210f4754a1a249420e84816fc6e26c616
Rafael Cronemberger <rafael@berger.com.br>
2023-07-15 16:53:21 -0300

    Packet updates with build yamls

---
+++ src/Berger.Extensions.International/Brazil/CnpjHelper.cs
@@ -0,0 +1,56 @@
+﻿namespace Berger.Extensions.International
+{
+    public static class CnpjHelper
+    {
+        public static bool CheckCnpj(string cnpj)
+        {
+            if (string.IsNullOrEmpty(cnpj))
+                return false;
+
+            int[] multiplier_1 = new int[12] { 5, 4, 3, 2, 9, 8, 7, 6, 5, 4, 3, 2 };
+            int[] multiplier_2 = new int[13] { 6, 5, 4, 3, 2, 9, 8, 7, 6, 5, 4, 3, 2 };
+
+            var rest = 0;
+            var total = 0;
+            var digit = string.Empty;
+            var temporary = string.Empty;
+
+            cnpj = cnpj.Trim();
+            cnpj = cnpj.Replace(".", "").Replace("-", "").Replace("/", "");
+
+            if (cnpj.Length != 14)
+                return false;
+
+            temporary = cnpj.Substring(0, 12);
+            total = 0;
+
+            for (int i = 0; i < 12; i++)
+                total += int.Parse(temporary[i].ToString()) * multiplier_1[i];
+
+            rest = (total % 11);
+
+            if (rest < 2)
+                rest = 0;
+            else
+                rest = 11 - rest;
+
+            digit = rest.ToString();
+            temporary = temporary + digit;
+            total = 0;
+
+            for (int i = 0; i < 13; i++)
+                total += int.Parse(temporary[i].ToString()) * multiplier_2[i];
+
+            rest = (total % 11);
+
+            if (rest < 2)
+                rest = 0;
+            else
+                rest = 11 - rest;
+
+            digit = digit + rest.ToString();
+
+            return cnpj.EndsWith(digit);
+        }
+    }
+}
---
+++ src/Berger.Extensions.International/Brazil/CpfHelper.cs
@@ -0,0 +1,79 @@
+﻿namespace Berger.Extensions.International
+{
+    public static class CpfHelper
+    {
+        public static bool CheckCpf(string cpf)
+        {
+            if (string.IsNullOrEmpty(cpf))
+                return false;
+
+            int[] multiplier_1 = new int[9] { 10, 9, 8, 7, 6, 5, 4, 3, 2 };
+            int[] multiplier_2 = new int[10] { 11, 10, 9, 8, 7, 6, 5, 4, 3, 2 };
+
+            var rest = 0;
+            var total = 0;
+
+            var digit = string.Empty;
+            var temporary = string.Empty;
+            var verifiers = string.Empty;
+
+            cpf = cpf.Trim();
+            cpf = cpf.Replace(".", "").Replace("-", "");
+
+            var invalids = new List<string>();
+
+            invalids.Add("00000000000");
+            invalids.Add("11111111111");
+            invalids.Add("22222222222");
+            invalids.Add("33333333333");
+            invalids.Add("44444444444");
+            invalids.Add("55555555555");
+            invalids.Add("66666666666");
+            invalids.Add("77777777777");
+            invalids.Add("88888888888");
+            invalids.Add("99999999999");
+
+            if (invalids.Contains(cpf))
+                return false;
+
+            if (cpf.Length != 11)
+                return false;
+
+            temporary = cpf.Substring(0, 9);
+            total = 0;
+
+            for (int i = 0; i < 9; i++)
+                total += int.Parse(temporary[i].ToString()) * multiplier_1[i];
+
+            rest = total % 11;
+
+            if (rest < 2)
+                rest = 0;
+            else
+                rest = 11 - rest;
+
+            digit = rest.ToString();
+            verifiers = digit;
+            temporary = temporary + digit;
+            total = 0;
+
+            for (int i = 0; i < 10; i++)
+                total += int.Parse(temporary[i].ToString()) * multiplier_2[i];
+
+            rest = total % 11;
+
+            if (rest < 2)
+                rest = 0;
+            else
+                rest = 11 - rest;
+
+            digit = rest.ToString();
+            verifiers += digit;
+
+            if ((verifiers) == cpf.Remove(0, 9))
+                return true;
+            else
+                return false;
+        }
+    }
+}
